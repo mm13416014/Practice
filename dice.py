6d4edb0f9f3f9a0785ba03cefdca95308ab151c8
mm13416014 <japanpublicnetwork@gmail.com>
2025-06-23 22:04:19 +0900

    first commit

---
+++ dice.py
@@ -0,0 +1,12 @@
+import random
+
+class Dice:
+  
+  face_num =6
+  
+  def __init__(self):
+    print("Hello")
+  
+  def shoot(self):  
+    return random.randint(1,self.face_num)
+  
---
+++ test.py
@@ -0,0 +1,9 @@
+test_list = [1,2,3]
+len(test_list)
+
+length = 0
+
+for i in test_list:
+  length += 1
+
+print(length)
